2f714658e6906d000ae80639ec761ef44fe282f5
CodeSpec <online.codespec@gmail.com>
2024-03-16 16:25:55 +0530

    Home page all issues resolved, scroll issue 4.0, down button issue 3.0

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image"
 import { motion } from "framer-motion"
+import Link from "next/link"
 
 const page = () => {
   return (
@@ -12,7 +13,7 @@ const page = () => {
       transition={{ duration: 1 }}
     >
 
-      <div className="h-screen w-auto overflow-scroll flex flex-col px-4 sm:px-8 md:px-12 lg:px-24 lg:flex-row">
+      <div className="h-full flex flex-col gap-0 px-4 py-10 box-border sm:px-8 md:px-12 md:py-0 lg:px-24 lg:flex-row lg:w-full">
 
         {/* Image Container */}
         <div className="h-1/2 relative lg:h-full lg:w-1/2 z-20">
@@ -20,21 +21,25 @@ const page = () => {
         </div>
 
         {/* Text Container */}
-        <div className="h-1/2 flex flex-col gap-8 justify-center items-center lg:w-1/2 lg:h-full">
+        <div className="h-1/2 flex flex-col gap-8 justify-between items-center lg:justify-center lg:gap-12 lg:w-1/2 lg:h-full">
 
           {/* Title */}
-          <h1 className="text-4xl font-bold md:text-6xl">Crafting Digital Experiences, Designing Tomorrow</h1>
+          <h1 className="text-4xl text-center font-bold md:text-6xl">Crafting Digital Experiences, Designing Tomorrow</h1>
 
           {/* Description */}
-          <p className="md:text-xl lg:w-full">Welcome to my digital canvas</p>
+          <p className="text-center md:text-xl lg:w-full">Welcome to my digital canvas</p>
 
           {/* Buttons */}
-          <div className="flex gap-4 lg:w-full">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View my work</button>
-            <button className="p-4 rounded-lg ring-1 ring-black">Contact me</button>
+          <div className="flex gap-4 justify-around lg:justify-center lg:gap-12 lg:w-full">
+           
+            <Link href='/portfolio' className="ring-1 ring-black bg-black text-white bg-gradient-to-br from-white-opacity-10 via-transparent px-6 py-3 rounded-lg shadow-gray-950/30 shadow-lg backdrop-blur-[10px] transition-all duration-300 hover:scale-110">View my work</Link>
+            
+            <Link href='/contact' className="ring-1 ring-black bg-gradient-to-br from-white-opacity-10 via-transparent px-6 py-3 rounded-lg shadow-gray-950/30 shadow-lg backdrop-blur-[10px] transition-all duration-300 hover:scale-110">Contact me</Link>
           </div>
         </div>
       </div>
+
+      <div className="h-2 lg:hidden pt-8"></div>
     </motion.div>
   )
 }
